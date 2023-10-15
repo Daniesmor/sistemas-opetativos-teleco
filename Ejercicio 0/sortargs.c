0a8f60063b534f33c3f5ed0f563c1ef77522fb61
Daniesmor <daniesmor@gmail.com>
2023-10-15 13:21:46 +0200

    sortargs con doble algoritmo

--- Ejercicio 0/sortargs.c
+++ Ejercicio 0/sortargs.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+enum {
+	Distance = 'a' - 'A',
+};
 
 void
 capitalizer(int argc, char *argv[])
@@ -7,14 +12,14 @@ capitalizer(int argc, char *argv[])
 	for (int i = 1; i < argc; i++) {
 		for (int b = 0; argv[i][b] != '\0'; b++) {
 			if (argv[i][b] >= 'a' && argv[i][b] <= 'z') {
-				argv[i][b] = argv[i][b] - 32;
+				argv[i][b] = argv[i][b] - Distance;
 			}
 		}
 	}
 }
 
 void
-sort(int argc, char *argv[])
+insertion_sort(int argc, char *argv[])
 {
 	for (int i = 2; i < argc; i++) {
 		char *z = argv[i];
@@ -30,15 +35,30 @@ sort(int argc, char *argv[])
 	}
 }
 
+void
+selection_sort(int argc, char *argv[])
+{
+	for (int i = 1; i < argc; i++) {
+		for (int j = i + 1; j < argc; j++) {
+			char *z = argv[i];
+
+			if (strcmp(argv[i], argv[j]) > 0) {
+				argv[i] = argv[j];
+				argv[j] = z;
+			}
+		}
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
 	if (argc == 1) {
 		printf("Error: No arguments\n");
-		return 1;
+		exit(-1);
 	} else {
 		capitalizer(argc, argv);
-		sort(argc, argv);
+		insertion_sort(argc, argv);
 		for (int i = 1; i < argc; i++) {
 			printf("%s\n", argv[i]);
 

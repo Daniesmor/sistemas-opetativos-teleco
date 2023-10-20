c1c708c6c1a250ef45e06a14b290860478a626a6
Daniesmor <daniesmor@gmail.com>
2023-10-20 23:54:49 +0200

    siguientes lineas ej1

--- Ejercicio 1/watchdirs.c
+++ Ejercicio 1/watchdirs.c
@@ -13,13 +13,31 @@ void imprimirFecha() {
 int
 main (int argc, char *argv[])
 {
-    int father_pid = getpid();
+    pid_t father_pid = getpid();
+    pid_t child_pid;
     while (true) {
         
         imprimirFecha();
         printf("Watchdirs started (PID: %d)\n", father_pid);
-        int execl(const char *path, const char *arg, ...);
+        int execl(const char *path, const char *arg);
         sleep(3);
+    }
+    for (int i = 1; i < argc; i++) {
+        child_pid = fork();
+
+        if (child_pid == -1) {
+            perror("fork"); // Manejar error de creación de proceso hijo
+            return 1;
+        }
+
+        if (child_pid == 0) {
+            // Código del proceso hijo
+            printf("Soy el proceso hijo (PID: %d)\n", getpid());
+        } else {
+            // Código del proceso padre
+            printf("Soy el proceso padre (PID: %d)\n", getpid());
+        }
+
     }
 
 }

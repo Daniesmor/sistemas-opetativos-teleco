6b10c7b067614f3e57a92e92089bc7996813a44c
Daniesmor <daniesmor@gmail.com>
2023-11-29 22:46:47 +0100

    numerosos cambios con buenas practicas de dup2 y execl

--- Ejercicio5/shafiles.c
+++ Ejercicio5/shafiles.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <err.h>
+#include <sys/wait.h>
 
 // ----------------------- ESTRUCTURAS DE DATOS UTILIZADAS --------------------------------------------------------------------------------------
 
@@ -78,9 +79,9 @@ filename_extractor(char *line) {
         filename[filename_crt] = extension[c];
         filename_crt++;
     }
-
+    
     return filename;
-
+    
 
 }
 
@@ -97,31 +98,67 @@ clean_line(char *line) {
 
 
 void 
-sha_create(char *line, int *status) {
+sha_create(char *line, int *status, int fd) {
 
-    int exists = open(line, READ_PERMISSION);
-    //file_creation_check(exists); // SI EL ARCHIVO QUE SE DESEA ENCRIPTAR NO EXISTE SE ESCRIBIRA ERROR POR LA SALIDA
     
-    if (exists <= 0) {
-        *status = 1;
-    }
-    close(exists);
-
     int file_pid;
     switch (file_pid = fork()) {
-	case -1:
-		err(EXIT_FAILURE, "fork failed");
-	case 0:
+        case -1:
+            err(1, "fork failed");
+        case 0:
+            if (dup2(fd, STDOUT_FILENO) == -1) {
+                perror("Error to STDOUT redirection.");
+                exit(1);
+            }
 
-        // SI EXISTE SE ESCRIBIRÁ POR LA SALIDA EL SHA DEL ARCHIVO
-        if (exists >= 0) { 
-            execl("/bin/sha1sum", "sha1sum", line, NULL); //execl debe recibir un puntero que apunte a donde está el argumento
-            err(EXIT_FAILURE, "exec failed");
-        } else {
-            exit(1);
-        }          
-            
-	}
+            int exists = open(line, READ_PERMISSION);
+            if (exists < 0) {
+                *status = 1;
+                printf("ERROR");
+                exit(1);
+            }
+
+            if (exists >= 0) {
+                // Cerrar el descriptor de archivo no necesario
+                close(fd);
+
+                // Redirigir la salida estándar al archivo
+                if (dup2(exists, STDIN_FILENO) == -1) {
+                    perror("Error to STDIN redirection.");
+                    close(exists);
+                    exit(1);
+                }
+
+                // Ejecutar sha1sum
+                execl("/usr/bin/sha1sum", "sha1sum", NULL); // Ruta a sha1sum puede variar
+                perror("execl failed");
+                close(exists);
+                exit(1);
+            }
+            close(exists);
+            exit(0);
+        default:
+            int child_status;
+
+            if (wait(&child_status) == -1) {
+                perror("wait");
+                // Manejar el error aquí, por ejemplo, retornar un código de error o salir del programa
+            } else {
+                if (WIFEXITED(child_status)) {
+                    // El proceso hijo ha terminado normalmente
+                    printf("ESto vale el status %d \n", WEXITSTATUS(child_status));
+                    if (WEXITSTATUS(child_status) > 0) {
+                        *status = WEXITSTATUS(child_status);
+                    }
+                    
+                } else {
+                    // El proceso hijo no terminó normalmente (por ejemplo, fue interrumpido)
+                    // Puede manejar este caso dependiendo de su lógica de programa
+                }
+            }
+                
+    }
+
    
 }
 
@@ -132,34 +169,44 @@ create_files(char *line, int *status) {
     // CREAMOS EL ARCHIVO NUEVO CON EL NOMBRE DEL ARCHIVO A ENCRIPTAR
     int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0640);
     file_creation_check(fd);
-
-    // REDIRIGIMOS LA SALIDA ESTANDAR
-    if (dup2(fd, STDOUT_FILENO) == -1) {
-        perror("Error to STDOUT redirection.");
-    } else {
-        sha_create(line, status);
-    }
+    
+    sha_create(line, status, fd);
+        
+    
+    
+    
     // CERRAMOS ARCHIVO A ENCRIPTAR
     close(fd);
     free(filename);
+
 }
 
 void
 read_lines(int *status) {
 
     char *line = (char *)malloc(LINE_BUFFER_SIZE); // Hacemos una asignación inicial de memoria de 256 caracteres por linea
+    
     malloc_check(line);
     
     while (fgets(line, LINE_BUFFER_SIZE, stdin) != NULL) { //Cada vez que llamemos a fgets, se sobreescribirá line
 
         // EJECUTAMOS EL COMANDO SI RETORNA ERROR ES QUE EL ARCHIVO NO EXISTE POR LO QUE NO HABRA QUE CREAR UNO NUEVO
         line = clean_line(line); //Limpiamos line porque alfinal tiene un '\n'
+        //printf("line: %s \n", line);
         create_files(line, status);
+        
 
     }
 
-    free(line); // Liberamos la memoria
+    if (!feof(stdin)) {
+        // Llegamos al final de la entrada estándar
+        errx(EXIT_FAILURE, "eof not reached");
+        
+    } 
 
+
+    free(line);
+    
 }
 
 
